2f1970106e8734324cb716fc4f6967d37dd84663
Banidwivedi <aradhanadwivedi1968@gmail.com>
2023-10-18 21:56:15 -0700

    this is an astrology based numerology calculator where you might get an insight of your destiny , future , personality career and much more
    new
    
    new format
    
    new

---
+++ src/Info.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Info extends JFrame{
+
+
+    private JLabel lbWelcome;
+    private JLabel lbDescription;
+    public Info()
+    {
+        setSize(800, 600);
+    }
+
+}
+
